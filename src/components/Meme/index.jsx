548103589f8de26fa9916dfbe5553dae66e7df48
jaquesaurelio2 <jaquesaurelio1@gmail.com>
2024-06-29 17:16:26 -0300

    Initial commit

---
+++ src/components/Meme/index.jsx
@@ -0,0 +1,49 @@
+import styles from "./index.module.css";
+import memesData from "../../memesData";
+import { useState } from "react";
+
+export default function Meme() {
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    image: "https://i.imgflip.com/43a45p.png"
+  });
+
+  const getNewMeme = () => {
+    const memes = memesData.data.memes;
+
+    const randomIndex = Math.floor(Math.random() * memes.length);
+
+    const url = memes[randomIndex].url;
+
+    setMeme((oldMeme) => ({
+      ...oldMeme,
+      image: url
+    }));
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.form}>
+        <input
+          type="text"
+          className={styles.input}
+          placeholder="Texto superior"
+        />
+        <input
+          type="text"
+          className={styles.input}
+          placeholder="Texto inferior"
+        />
+        <button onClick={getNewMeme} className={styles.button}>
+          Obter novo meme 🖼
+        </button>
+      </div>
+      <div className={styles.meme}>
+        <img src={meme.image} alt="meme" />
+        <h2 className={styles.topText}></h2>
+        <h2 className={styles.bottomText}></h2>
+      </div>
+    </div>
+  );
+}
